5e9e045c6abf0616fa3b5eb472bffb8fe721e708
daniarfadd <daniarfadaffa@yahoo.com>
2022-07-07 17:22:41 +0700

    making project structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import Projects from './pages/Projects'
+import Experience from './pages/Experience'
+
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/experience' element={<Experience />} />
+        </Routes>
+      </Router>
+      <h1>Hello</h1>
     </div>
   );
 }
